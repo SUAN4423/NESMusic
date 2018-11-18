8268eafa6d1fe83bc22c24c44b59dccd2143ca06
unknown <toyanonichan@yahoo.co.jp>
2018-11-18 16:53:58 +0900

    16進数表記の変更

--- src/suan/mydns/jp/Thoone.java
+++ src/suan/mydns/jp/Thoone.java
@@ -53,7 +53,7 @@ public class Thoone extends PApplet
 	private boolean pressed = false;
 	public static Version ver = new Version();
 
-	public static final String Version = "1.8.5";
+	public static final String Version = "1.8.6";
 	public static String newVersion = Version;
 
 	DropTarget dropTarget;
